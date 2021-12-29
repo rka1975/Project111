09f5c6fd0220ef578646cccd1293e4fc0c95bb76
rka1975 <rupeshkagrawal@gmail.com>
2021-12-28 23:44:36 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -35,3 +35,46 @@ function speak(){
   var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data_2);
   synth.speak(utterThis);
 }
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML= results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML= results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label=="Peace")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128512";
+        }
+        if(results[0].label=="Okay")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128532";
+        }
+        if(results[0].label=="Thumbs Up")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128545";
+        }
+        if(results[1].label=="Peace")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#128512";
+        }
+        if(results[1].label=="Okay")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#128532";
+        }
+        if(results[1].label=="Thumbs Up")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#128545";
+        }
+    }
+}
